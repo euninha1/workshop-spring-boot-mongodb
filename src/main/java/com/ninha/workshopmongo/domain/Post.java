498b45268959b85738e953918d0204fa6cd231b4
Fernanda Amaral <fernanda.amaral00@souunit.com.br>
2020-07-16 15:53:38 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/ninha/workshopmongo/domain/Post.java
+++ src/main/java/com/ninha/workshopmongo/domain/Post.java
@@ -2,11 +2,14 @@ package com.ninha.workshopmongo.domain;
 
 import java.io.Serializable;
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.ninha.workshopmongo.dto.AuthorDTO;
+import com.ninha.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -18,6 +21,8 @@ public class Post implements Serializable{
 	private String title; 
 	private String body; 
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>(); 
 	
 	public Post() {	
 	}
@@ -70,6 +75,14 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -95,4 +108,6 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
+
+
 }
