b2941814d5e09a3a11c6952069d3e0eda7027a20
Fernanda Amaral <fernanda.amaral00@souunit.com.br>
2020-07-16 15:09:51 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/ninha/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ninha/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,10 @@ package com.ninha.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ninha.workshopmongo.domain.User;
-
+import com.ninha.workshopmongo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
-	User findOne(String id);	
+
 }
