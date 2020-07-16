0dad2bac5c584077fb46a169e317b17059289f3b
Fernanda Amaral <fernanda.amaral00@souunit.com.br>
2020-07-16 15:23:57 -0300

    Referenciando os posts do usuario

--- src/main/java/com/ninha/workshopmongo/domain/User.java
+++ src/main/java/com/ninha/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.ninha.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -13,6 +16,9 @@ public class User implements Serializable {
 	private String id; 
 	private String name; 
 	private String email; 
+	
+	@DBRef
+	private List<Post> posts = new ArrayList<>();
 	
 	public User () { 
 	}
@@ -44,6 +50,15 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 
 	@Override
 	public int hashCode() {
@@ -81,4 +96,5 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
 }
