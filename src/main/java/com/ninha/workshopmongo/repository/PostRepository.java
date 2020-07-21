7a5dec80e5814c330739c4ede6f2ba84199279bd
Fernanda Amaral <fernanda.amaral00@souunit.com.br>
2020-07-21 11:39:27 -0300

    consulta simples com query methods

--- src/main/java/com/ninha/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ninha/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.ninha.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,6 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 	Post findOne(String id);
 
-
+	List<Post> findByTitleContainingIgnoreCase(String text); 
+	
 }
--- src/main/java/com/ninha/workshopmongo/resource/PostResource.java
+++ src/main/java/com/ninha/workshopmongo/resource/PostResource.java
@@ -1,13 +1,17 @@
 package com.ninha.workshopmongo.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ninha.workshopmongo.domain.Post;
+import com.ninha.workshopmongo.resource.util.URL;
 import com.ninha.workshopmongo.services.PostService;
 
 @RestController
@@ -24,4 +28,11 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="")String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text); 
+		return ResponseEntity.ok().body(list);
+
+	}
 }
